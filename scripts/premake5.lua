44170b10d07dd6d85c6de8cbc8e494f5c087dedd
SimpleGameEngine <asdfasdfasdfasdf@asdf.pl>
2018-12-11 22:35:42 +0100

    Add glm and glew.

--- scripts/premake5.lua
+++ scripts/premake5.lua
@@ -28,19 +28,25 @@ project "GameEngine"
     includedirs {
         ROOT_PATH .. "/source/",
         ROOT_PATH .. "/third_party/SDL2-2.0.9/include/",
+        ROOT_PATH .. "/third_party/glew-2.1.0/include/",
+        ROOT_PATH .. "/third_party/glm-0.9.9.3/include/",
     }
 
     libdirs {
         ROOT_PATH .. "/third_party/SDL2-2.0.9/lib/x86/",
+        ROOT_PATH .. "/third_party/glew-2.1.0/lib/x86/",
     }
 
     links {
         "SDL2.lib",
         "SDL2main.lib",
+        "glew32.lib",
+        "opengl32.lib"
     }
 
     postbuildcommands {
         "{COPY} " .. ROOT_PATH .. "/third_party/SDL2-2.0.9/bin/x86/SDL2.dll %{cfg.targetdir}",
+        "{COPY} " .. ROOT_PATH .. "/third_party/glew-2.1.0/bin/x86/glew32.dll %{cfg.targetdir}",
     }
 
     filter "configurations:Debug"
